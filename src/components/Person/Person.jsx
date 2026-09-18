bf93ce371e6e3c0afab10f609e1267a3e312ca06
Anton Floreskul <floreskula830@gmail.com>
2026-09-18 12:15:27 +0300

    fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,23 @@
-function getPartnerLabel(person) {
-  if (!person.isMarried) {
+function getPartnerLabel(sex, isMarried, partnerName) {
+  if (!isMarried) {
     return 'I am not married';
   }
 
-  return person.sex === 'f'
-    ? `${person.partnerName} is my husband`
-    : `${person.partnerName} is my wife`;
+  return sex === 'f'
+    ? `${partnerName} is my husband`
+    : `${partnerName} is my wife`;
 }
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    <p className="Person__partner">{getPartnerLabel(person)}</p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">
+        {getPartnerLabel(sex, isMarried, partnerName)}
+      </p>
+    </section>
+  );
+};
